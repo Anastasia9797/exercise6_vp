d46b609881c3169a1e40416953edc0875eacb399
Anastasia9797 <n6609400@gmail.com>
2025-04-23 19:45:00 +0300

    Example3

--- exercise6_vp/MainWindow.xaml.cs
+++ exercise6_vp/MainWindow.xaml.cs
@@ -22,11 +22,11 @@ namespace exercise6_vp
             InitializeComponent();
 
             List<User> users = new List<User>();
-            users.Add(new User() { ID = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23)} );
+            users.Add(new User() { ID = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
             users.Add(new User() { ID = 2, Name = "Katy Doe", Birthday = new DateTime(1973, 1, 27) });
             users.Add(new User() { ID = 3, Name = "Emma Doe", Birthday = new DateTime(1997, 10, 12) });
 
-            dgSimple.ItemsSource = users;
+            dgUsers.ItemsSource = users;
         }
 
         public class User
@@ -34,6 +34,14 @@ namespace exercise6_vp
             public int ID { get; set; }
             public string Name { get; set; }
             public DateTime Birthday { get; set; }
+
+            public string Details
+            {
+                get
+                {
+                    return String.Format("{0} народився/народилась {1}", this.Name, this.Birthday.ToLongDateString());
+                }
+            }
         }
     }
 }
