4ccef8cb3cedb378e155b928b00e95ebee235d9b
Anastasia9797 <n6609400@gmail.com>
2025-04-23 17:47:17 +0300

    Добавьте файлы проекта.

---
+++ exercise6_vp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace exercise6_vp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<User> users = new List<User>();
+            users.Add(new User() { ID = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23)} );
+            users.Add(new User() { ID = 2, Name = "Katy Doe", Birthday = new DateTime(1973, 1, 27) });
+            users.Add(new User() { ID = 3, Name = "Emma Doe", Birthday = new DateTime(1997, 10, 12) });
+
+            dgSimple.ItemsSource = users;
+        }
+
+        public class User
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public DateTime Birthday { get; set; }
+        }
+    }
+}
